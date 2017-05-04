b7ebb962a1cd202d2b293d8b64f043c239d32728
User <User>
2017-05-04 13:11:19 +0400

    first commit

---
+++ cavidan_test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cavidan_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("reqem daxil edin: ");
+            int reqem = Convert.ToInt16(Console.ReadLine());
+            int netice = (reqem % 2 == 0) ? netice = reqem * reqem : netice = reqem * reqem * reqem;
+            Console.WriteLine("netice: {0}",netice);
+            Console.ReadLine();
+            
+        }
+    }
+}
